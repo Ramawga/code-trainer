3ab7f4fe0194456c12ce25e2e8210e3643a71bb0
Ramawga <lucascavalcante023@gmail.com>
2026-03-14 17:40:49 -0300

    feat:creating challengeGenerator page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./styles/global.scss";
 import Header from "./components/header";
 import Footer from "./components/footer/indext";
+//pages
+import ChallengeGenerator from "./pages/challengeGenerator";
 function App() {
   return (
     <>
       <Header />
+      <ChallengeGenerator/>
       <Footer />
     </>
   );
---
+++ src/pages/challengeGenerator/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.scss";
+const ChallengeGenerator = () => {
+  return (
+    <section className="challengeGenerator">
+      <div className="wrapper">
+    <div className="w60"></div >
+    <div className="w40"></div>
+      </div>
+  
+    </section>
+  )
+}
+
+export default ChallengeGenerator
