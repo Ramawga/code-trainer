49a95889cf04960ece62308e8b446be20bdbe984
Ramawga <lucascavalcante023@gmail.com>
2026-03-14 20:17:09 -0300

    fix: fix title

--- src/components/LanguagesSelector/index.tsx
+++ src/components/LanguagesSelector/index.tsx
@@ -4,7 +4,7 @@ const LanguagesSelector = () => {
   return (
      <div className="LanguagesSelector">
       <div className="LanguagesSelector__wrapper">
-        <div className="texts"><p className="title">LanguagesSelector</p> <p className="supportText">Choose target level</p></div>
+        <div className="texts"><p className="title">Languages</p> <p className="supportText">Choose target level</p></div>
         <div className="LanguagesButtons">
           <button className="Language">JavaScript</button>
           <button className="Language">Python</button>
